d0348eb4b6f5469de9614c8f77bd07ea57636d87
Andrey <takemyenergy55@gmail.com>
2019-01-26 20:35:04 +0500

    sanya

--- meow/settings.py
+++ meow/settings.py
@@ -52,7 +52,11 @@ CORS_ORIGIN_WHITELIST =[
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
 
 # Application definition
