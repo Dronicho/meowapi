0d3f4a59614b56112e60e194b710796c2e220b70
Andrey Nebogatikov <takemyenergy55@gmail.com>
2019-01-29 17:29:45 +0500

    secure

--- meowapi/models.py
+++ meowapi/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Article(models.Model):
     url_to_source = models.CharField(max_length=200)
-    title = models.TextField(max_length=200, default='none')
-    summary = models.TextField(max_length=500, default='none')
+    title = models.TextField(max_length=5000, default='none')
+    summary = models.TextField(max_length=10000, default='none')
     pub_date = models.DateTimeField('date published')
     # tags = ArrayField(models.CharField, size=10)
 # Create your models here.
